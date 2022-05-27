70f4b61c41bda25c1e08978b432b4eb75636be38
tomasamayaa1 <tomasamayaa1@gmail.com>
2022-05-27 19:45:04 -0300

    levantando servidor con app.js y creacion de carpetas

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static('public'))
+
+app.listen(3000, () => {
+    console.log('server on');
+});
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'))
+})
+
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/productCart.html'))
+})
+
+app.get('/producto', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/productDetail.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'))
+})
+
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'))
+})
+
+
+
+
+
