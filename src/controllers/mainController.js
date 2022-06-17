92364731c1be1d042f213a2618837a83686c038d
aruuleon <arieleonchang@gmail.com>
2022-06-17 19:25:03 -0300

    Mantenimiento de nuevas carpetas y archivos creados (MVC)

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,38 +1,24 @@
 const mainController = {
     index: function (req, res) {
-        res.render('./users/index', {
-            nombre: "index"
-})
+        res.render('./users/index', { nombre: 'index' });
     },
     login: function (req, res) {
-        res.render('./users/login', {
-            nombre: "login"
-        })
+        res.render('./users/login', { nombre: 'login' });
     },
     productCar: function (req, res) {
-        res.render('./users/productCar', {
-            nombre: "productCar" 
-        })
+        res.render('./users/productCar', { nombre: 'productCar' });
     },
     productCreation: function (req, res) {
-        res.render('./products/productCreation', {
-            nombre: "productCreation"
-        })
+        res.render('./products/productCreation', { nombre: 'productCreation' });
     },
     productDetail: function (req, res) {
-        res.render('./users/productDetail', {
-            nombre: "productDetail"
-        })
+        res.render('./users/productDetail', { nombre: 'productDetail' });
     },
     productEdition: function (req, res) {
-        res.render('./products/productEdition', {
-        nombre: "productEdition"
-        })
+        res.render('./products/productEdition', { nombre: 'productEdition' });
     },
     register: function (req, res) {
-        res.render('./users/register', {
-            nombre: "register"
-        })
+        res.render('./users/register', { nombre: 'register' });
     },
 };
 
