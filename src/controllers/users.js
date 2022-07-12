a2e5e196b42892401dd29399b36805184b043b05
tomasamayaa1 <tomasamayaa1@gmail.com>
2022-07-12 18:21:48 -0300

    comenzando sprint 5

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,12 +1,42 @@
 const fs = require('fs');
 const path = require('path');
+const bcryptjs = require('bcryptjs')
 
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const usersFilePath = path.join(__dirname, '../data/usersDataBase.json');
+let users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
+const categoriesFilePath = path.join(__dirname, '../data/categoriesDataBase.json');
+let categories = JSON.parse(fs.readFileSync(categoriesFilePath, 'utf-8'));
 
 const controller = {
+    buscarUsuario: (id) => {
+		let usuarioEncontrado;
+		for (const user of users) {
+			if (id == user.id) {
+				usuarioEncontrado = user;
+			}
+		}
+		return usuarioEncontrado;
+	},
+    guardarEnArchivo: () => {
+		let userJSON = JSON.stringify(users);
+		fs.writeFileSync(usersFilePath, userJSON);
+	},
     login: function (req, res) {
         res.render('./users/login', { name: 'login', title : 'LOGIN' });
+    },
+    procesoLogin: function (req, res){
+        let emailEncontrado = req.body.email;
+		for (const user of users) {
+			if (emailEncontrado == user.email) {
+                let comparar = bcryptjs.compareSync(req.body.password, user.password);
+                if (comparar) {
+                    res.render('./users/categories', { categories, name: 'categories', title: 'CATEGORIAS' });
+                } else {
+                    res.render('./users/login', { name: 'login', title: 'LOGIN' });
+                }
+            }
+		}
     },
     index: function (req, res) {
         res.render('./users/login', { name: '', title : '' });
@@ -18,17 +48,32 @@ const controller = {
         res.render('./users/register', { name: 'register', title : 'REGISTRARSE' });
     },
     store: function (req, res) {
-        res.render('./users/login', { name: '', title : '' });
+        let usuarioAgregado = req.body;
+        usuarioAgregado.password = bcryptjs.hashSync(req.body.password, 10);
+		usuarioAgregado.id = users.length + 1;
+        usuarioAgregado.image = req.file.filename;
+        users.push(usuarioAgregado);
+		controller.guardarEnArchivo();
+		res.redirect('/');
     },
     edit: function (req, res) {
         res.render('./users/login', { name: '', title : '' });
     },
     update: function (req, res) {
+        let usuarioEditado = controller.buscarUsuario(req.params.id);
+		let unArchivo = req.file;
+		let variosArchivos = req.files;
+		usuarioEditado.firstname = req.body.firstname;
+		usuarioEditado.lastname = req.body.lastname;
+		usuarioEditado.phone = req.body.phone;
+		usuarioEditado.image = req.file.filename;
+	
         res.render('./users/register', { name: '', title : '' });
     },
     delete: function (req, res) {
         res.render('./users/login', { name: '', title : '' });
     },
 };
+
 
 module.exports = controller;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination : (req, file, cb) => {
+        cb(null, path.join(__dirname, '../../public/img/users'));
+    },
+
+    filename : (req, file, cb) => {
+        let date = Date.now();
+        let random = Math.round(Math.random() * 1E9);
+        let extname = path.extname(file.originalname);
+        const newFileName = `img-${date}${random}${extname}`;
+        cb(null, newFileName);
+    },
+});
+const upload = multer({ storage });
 
 const usersController = require('../controllers/users');
 
 /*** USER LOGIN ***/ 
 router.get('/login', usersController.login);
+router.post('/login', usersController.procesoLogin);
 
 /*** GET ALL USERS ***/ 
 router.get('/users', usersController.index);
@@ -14,7 +32,7 @@ router.get('/detail/:id', usersController.detail);
 
 /*** REGISTER ONE USER ***/ 
 router.get('/register', usersController.register);
-router.post('/register', usersController.store);
+router.post('/register', upload.single('avatar'), usersController.store);
 
 /*** EDIT ONE USER ***/ 
 router.get('/edit/:id', usersController.edit);
