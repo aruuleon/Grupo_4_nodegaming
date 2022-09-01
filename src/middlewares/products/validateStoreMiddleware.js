8825e7c846c6ffc5fc8176336f6371edad190a5d
GerHw <german24hwang@gmail.com>
2022-09-01 17:50:23 -0300

    Validaciones en el back

--- src/middlewares/products/validateStoreMiddleware.js
+++ src/middlewares/products/validateStoreMiddleware.js
@@ -3,17 +3,17 @@ const path = require('path');
 const { body } = require('express-validator');
 
 module.exports = [
-    body('name').notEmpty().withMessage('Tenes que indicar un nombre'),
+    body('name').notEmpty().withMessage('Tenes que indicar un nombre').bail().isLength({min: 5}).withMessage('Debe tener al menos 5 caracteres'),
     body('brand_id').notEmpty().withMessage('Tenes que indicar una marca'),
     body('category_id').notEmpty().withMessage('Tenes que indicar una categoría'),
-    body('description').notEmpty().withMessage('Tenes que indicar una descripción'),
+    body('description').notEmpty().withMessage('Tenes que indicar una descripción').bail().isLength({min: 20}).withMessage('Debe tener al menos 20 caracteres'),
     body('price').notEmpty().withMessage('Tenes que indicar un precio'),
     body('discount').notEmpty().withMessage('Tenes que indicar un descuento'),
     body('stock').notEmpty().withMessage('Tenes que indicar un stock'),
 
     body(['image_primary', 'image_secondary', 'image_tertiary']).custom((value, { req }) => {
         let file = req.file;
-        let acceptedExtensions = ['.jpg', '.png', '.jpeg'];
+        let acceptedExtensions = ['.jpg', '.png', '.jpeg', '.gif'];
 
         if (!file) {
             throw new Error('Tenes que subir una imagen');
--- src/middlewares/users/validateRegisterMiddleware.js
+++ src/middlewares/users/validateRegisterMiddleware.js
@@ -3,24 +3,22 @@ const path = require('path');
 const { body } = require('express-validator');
 
 module.exports = [
-    body('firstname').notEmpty().withMessage('Tenes que escribir un nombre'),
-    body('lastname').notEmpty().withMessage('Tenes que escribir un apellido'),
+    body('firstname').notEmpty().withMessage('Tenes que escribir un nombre').bail().isLength({min: 2}).withMessage('Debe tener al menos 2 caracteres'),
+    body('lastname').notEmpty().withMessage('Tenes que escribir un apellido').bail().isLength({min: 2}).withMessage('Debe tener al menos 2 caracteres'),
     body('email').notEmpty().withMessage('Tenes que escribir un email').bail().isEmail().withMessage('Tenes que escribir un formato de email válido'),
-    body('phone').notEmpty().withMessage('Tenes que escribir un número telefónico'),
-    body('password').notEmpty().withMessage('Tenes que escribir una contraseña'),
+    body('phone').notEmpty().withMessage('Tenes que escribir un número telefónico').bail().isLength({min: 10}).withMessage('Debe tener al menos 10 numeros'),
+    body('password').notEmpty().withMessage('Tenes que escribir una contraseña').bail().matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i").withMessage('Debe tener al menos 8 caracteres ,1 mayuscula, 1 minuscula, 1 numero y 1 caracter especial'),
     
-    // body('avatar').custom((value, { req }) => {
-    //     let file = req.file;
-    //     let acceptedExtensions = ['.jpg', '.png', '.jpeg', '.gif'];
+    body('avatar').custom((value, { req }) => {
+        let file = req.file;
+        let acceptedExtensions = ['.jpg', '.png', '.jpeg', '.gif'];
 
-    //     if (!file) {
-    //         throw new Error('Tenes que subir una imagen');
-    //     } else {
-    //         let extname =  path.extname(file.originalname);
-    //         if (!acceptedExtensions.includes(extname)) {
-    //             throw new Error('Las extenciones de archivo permitidas son ' + acceptedExtensions.join(', '));
-    //         }
-    //     }
-    //     return true;
-    // }),
+        if (file) {
+            let extname =  path.extname(file.originalname);
+            if (!acceptedExtensions.includes(extname)) {
+                throw new Error('Las extenciones de archivo permitidas son ' + acceptedExtensions.join(', '));
+            }
+        }
+        return true;
+    }),
 ];
